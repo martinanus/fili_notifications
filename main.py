90f0aa1c4fc344bdd7ca2cf30adaf2cffbd4a0e2
Martin <manus@fi.uba.ar>
2023-03-16 20:00:31 -0300

    WIP adds trigger logic

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from email.mime.text import MIMEText
 
 import json
 
+
 def get_client():    
     credentials = service_account.Credentials.from_service_account_file(
         key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"],
@@ -32,7 +33,7 @@ def get_configuration():
     query_job       = client.query(query)  # Make an API request.
     result          = query_job.result()
     df              = result.to_dataframe();
-    df              = df.sort_values(by='Timestamp', ascending=True)
+    df              = df.sort_values(by='timestamp', ascending=True)
 
     return df
 
@@ -46,7 +47,7 @@ def get_notification_msgs():
 
 
 def get_days_as_list(column):
-    vals            = df_conf[column].values[-1]
+    vals            = df_config[column].values[-1]
     if (vals is None) or (vals[0:7] is 'Ninguna'):
         return []
     vals_trim       = re.sub("[^0-9,-]", "", vals)
@@ -57,7 +58,7 @@ def get_days_as_list(column):
 
 
 def get_mails_as_list():
-    vals            = df_conf["internal_email_notif"].values[-1]
+    vals            = df_config["internal_email"].values[-1]
     if (vals is None) or ('@' not in vals) or ('.' not in vals):
         return []        
     str_l           = vals.split(',')
@@ -79,6 +80,44 @@ def get_invoices_to_notify(relation, days_to_pay):
     
     return df;
 
+
+def get_pending_invoices():
+    
+    query = """
+    SELECT relation, counterpart, amount, invoice_id, due_date, contact_email, days_to_pay, notification_status
+    FROM `""" + invoice_table_id + """`
+    WHERE status!='aprobada'""";
+
+    query_job       = client.query(query)  
+    result          = query_job.result()
+    df              = result.to_dataframe();
+    
+    return df;
+
+
+def trigger_receipt_notif():
+    internal_pre_notif_days      = get_days_as_list("internal_pre_notif_collect")
+    internal_post_notif_days     = get_days_as_list("internal_post_notif_collect")
+    internal_post_notif_days     = ['-'+day for day in internal_post_notif_days]
+    internal_notif_days         = internal_pre_notif_days + internal_post_notif_days
+    
+    df = df_inv[(df_inv.relation=='Proveedor') & (df_inv.days_to_pay.isin(internal_notif_days))]
+    
+    return df.size 
+
+
+def trigger_payement_notif():
+    internal_pre_notif_days      = get_days_as_list("internal_pre_notif_pay")
+    internal_post_notif_days     = get_days_as_list("internal_post_notif_pay")
+    internal_post_notif_days     = ['-'+day for day in internal_post_notif_days]
+    
+    internal_notif_days         = internal_pre_notif_days + internal_post_notif_days
+    
+    df = df_inv[(df_inv.relation=='Cliente') & (df_inv.days_to_pay.isin(internal_notif_days))]
+    
+    return df.size 
+
+
 def format_html_table(table):
     table = table.replace('counterpart', 'Cliente')
     table = table.replace('amount', 'Monto ($)')
@@ -91,56 +130,60 @@ def format_html_table(table):
 
 def build_internal_receipt_notif_to_expire():
     body = '';
-    internal_pre_notif      = get_days_as_list("internal_pre_notif")
-    receipts_msgs           = notif_msgs["receipts_msgs"]
+    receipts_msgs       = notif_msgs["receipts_msgs"]
+    internal_pre_notif     = get_days_as_list("internal_pre_notif_collect")
+            
+    df                  = df_inv[(df_inv.relation=='Cliente') & 
+                            ( (df_inv.days_to_pay.isin(internal_pre_notif) | 
+                            ( (df_inv.notification_status=='notified') & (df_inv.days_to_pay >= 0)))) ]
 
-    if internal_pre_notif:            
-        df                  = get_invoices_to_notify('Cliente', internal_pre_notif)
-        df                  = df.sort_values(by='days_to_pay', ascending=True)
-                      
-        if '0' in internal_pre_notif: 
-            invoice_exp_today_l = df[df.days_to_pay == 0].reset_index();
-            if invoice_exp_today_l.size:
-                html_table      =  invoice_exp_today_l.to_html(columns=['counterpart', 'amount', 'invoice_id', 'due_date', 'contact_email'], justify='center')
-                html_table      = format_html_table(html_table)
-                body            += receipts_msgs["invoice_today_msg"]
-                body            += html_table
-                body            += receipts_msgs["hint_invoice_today_msg"]
-            else: 
-                body            += receipts_msgs["no_invoice_today_msg"]
-            internal_pre_notif.remove('0')
+    df                  = df.sort_values(by='days_to_pay', ascending=True)                  
+
+    invoice_exp_today_l = df[df.days_to_pay == 0].reset_index();
+    if invoice_exp_today_l.size:
+        html_table      =  invoice_exp_today_l.to_html(columns=['counterpart', 'amount', 'invoice_id', 'due_date', 'contact_email'], justify='center')
+        html_table      = format_html_table(html_table)
+        body            += receipts_msgs["invoice_today_msg"]
+        body            += html_table
+        body            += receipts_msgs["hint_invoice_today_msg"]
+    else: 
+        body            += receipts_msgs["no_invoice_today_msg"]
     
-        if internal_pre_notif:
-            invoice_pre_exp_l   = df[df.days_to_pay != 0].reset_index();
-            if invoice_pre_exp_l.size:
-                html_table      =  invoice_pre_exp_l.to_html(columns=['counterpart', 'amount', 'invoice_id', 'due_date', 'contact_email'], justify='center')
-                html_table      = format_html_table(html_table)
-                body            += receipts_msgs["invoice_today_msg"]
-                body            += html_table
-                body            += receipts_msgs["hint_invoice_today_msg"]
-            else: 
-                body            += receipts_msgs["no_invoice_today_msg"]
+    if '0' in internal_pre_notif: internal_pre_notif.remove('0')
+    
+    if internal_pre_notif:
+        invoice_pre_exp_l   = df[df.days_to_pay != 0].reset_index();
+        if invoice_pre_exp_l.size:
+            html_table      =  invoice_pre_exp_l.to_html(columns=['counterpart', 'amount', 'invoice_id', 'due_date', 'contact_email'], justify='center')
+            html_table      = format_html_table(html_table)
+            body            += receipts_msgs["invoice_to_expire_msg"]
+            body            += html_table
+            body            += receipts_msgs["hint_invoice_to_expire_msg"]
+        else: 
+            body            += receipts_msgs["no_invoice_to_expire_msg"]
 
     return body
 
 def build_internal_receipt_notif_expired():
     body = ''
-    internal_post_notif     = get_days_as_list("internal_post_notif")
+    internal_post_notif     = get_days_as_list("internal_post_notif_collect")
     internal_post_notif     = ['-'+day for day in internal_post_notif]
     receipts_msgs           = notif_msgs["payements_msgs"]
     
-    if internal_post_notif:    
-        df = get_invoices_to_notify('Cliente', internal_post_notif)        
-        invoice_expired_l   = df.sort_values(by='days_to_pay', ascending=True)
-        
-        if invoice_expired_l.size:
-            html_table      =  invoice_expired_l.to_html(columns=['counterpart', 'amount', 'invoice_id', 'due_date', 'contact_email'], justify='center')
-            html_table      = format_html_table(html_table)
-            body            += receipts_msgs["invoice_today_msg"]
-            body            += html_table
-            body            += receipts_msgs["hint_invoice_today_msg"]
-        else: 
-            body            += receipts_msgs["no_invoice_today_msg"]
+    df                  = df_inv[(df_inv.relation=='Cliente') & 
+                            ( (df_inv.days_to_pay.isin(internal_post_notif) | 
+                            ( (df_inv.notification_status=='notified') & (df_inv.days_to_pay < 0)))) ]                      
+    
+    invoice_expired_l   = df.sort_values(by='days_to_pay', ascending=True).reset_index()
+
+    if invoice_expired_l.size:
+        html_table      =  invoice_expired_l.to_html(columns=['counterpart', 'amount', 'invoice_id', 'due_date', 'contact_email'], justify='center')
+        html_table      = format_html_table(html_table)
+        body            += receipts_msgs["invoice_expired_msg"]
+        body            += html_table
+        body            += receipts_msgs["hint_invoice_expired_msg"]
+    else: 
+        body            += receipts_msgs["no_invoice_expired_msg"]
 
     return body
 
@@ -152,12 +195,6 @@ def build_internal_receipt_notif():
     mail_body += build_internal_receipt_notif_expired();
     mail_body += notif_msgs["ending_msg"]
     
-    mail_body = mail_body.replace('counterpart', 'Cliente')
-    mail_body = mail_body.replace('contact_email', 'E-mail')
-    mail_body = mail_body.replace('invoice_id', 'ID factura')
-    mail_body = mail_body.replace('due_date', 'Fecha de vencimiento')
-    mail_body = mail_body.replace('amount', 'Monto ($)')
-
     mail_data["body"]    = mail_body
     mail_data["to"]      = internal_email_notif
     mail_data["subject"] = notif_msgs["receipts_msgs"]["subject"]
@@ -167,56 +204,61 @@ def build_internal_receipt_notif():
 
 def build_internal_payements_notif_to_expire():
     body                    = ''
-    internal_pre_notif      = get_days_as_list("internal_pre_notif")
+    internal_pre_notif      = get_days_as_list("internal_pre_notif_pay")
     paymements_msgs         = notif_msgs["payements_msgs"]
-
-    if internal_pre_notif:            
-        df                  = get_invoices_to_notify('Proveedor', internal_pre_notif)
-        df                  = df.sort_values(by='days_to_pay', ascending=True)
-                        
-        if '0' in internal_pre_notif: 
-            invoice_exp_today_l = df[df.days_to_pay == 0].reset_index();
-            if invoice_exp_today_l.size:
-                html_table      =  invoice_exp_today_l.to_html(columns=['counterpart', 'amount', 'invoice_id', 'due_date', 'contact_email'], justify='center')
-                html_table      = format_html_table(html_table)
-                body            += paymements_msgs["invoice_today_msg"]
-                body            += html_table
-                body            += paymements_msgs["hint_invoice_today_msg"]
-            else: 
-                body            += paymements_msgs["no_invoice_today_msg"]
-            internal_pre_notif.remove('0');
     
-        if internal_pre_notif: 
-            invoice_pre_exp_l   = df[df.days_to_pay != 0].reset_index();
-            if invoice_pre_exp_l.size:
-                html_table      =  invoice_pre_exp_l.to_html(columns=['counterpart', 'amount', 'invoice_id', 'due_date', 'contact_email'], justify='center')
-                html_table      = format_html_table(html_table)
-                body            += paymements_msgs["invoice_to_expire_msg"]
-                body            += html_table
-                body            += paymements_msgs["hint_invoice_to_expire_msg"]
-            else: 
-                body            += paymements_msgs["no_invoice_to_expire_msg"]
+    df                  = df_inv[(df_inv.relation=='Proveedor') & 
+                            ( (df_inv.days_to_pay.isin(internal_pre_notif) | 
+                            ( (df_inv.notification_status=='notified') & (df_inv.days_to_pay >= 0)))) ]
+
+    df                  = df.sort_values(by='days_to_pay', ascending=True)
+                        
+
+    invoice_exp_today_l = df[df.days_to_pay == 0].reset_index();
+    if invoice_exp_today_l.size:
+        html_table      =  invoice_exp_today_l.to_html(columns=['counterpart', 'amount', 'invoice_id', 'due_date', 'contact_email'], justify='center')
+        html_table      = format_html_table(html_table)
+        body            += paymements_msgs["invoice_today_msg"]
+        body            += html_table
+        body            += paymements_msgs["hint_invoice_today_msg"]
+    else: 
+        body            += paymements_msgs["no_invoice_today_msg"]
+
+    if '0' in internal_pre_notif: internal_pre_notif.remove('0')
+
+    invoice_pre_exp_l   = df[df.days_to_pay != 0].reset_index();
+    if invoice_pre_exp_l.size:
+        html_table      =  invoice_pre_exp_l.to_html(columns=['counterpart', 'amount', 'invoice_id', 'due_date', 'contact_email'], justify='center')
+        html_table      = format_html_table(html_table)
+        body            += paymements_msgs["invoice_to_expire_msg"]
+        body            += html_table
+        body            += paymements_msgs["hint_invoice_to_expire_msg"]
+    else: 
+        body            += paymements_msgs["no_invoice_to_expire_msg"]
 
     return body
 
 def build_internal_payements_notif_expired():
     body = ''
-    internal_post_notif     = get_days_as_list("internal_post_notif")
+
+    internal_post_notif     = get_days_as_list("internal_post_notif_pay")
     internal_post_notif     = ['-'+day for day in internal_post_notif]
     paymements_msgs         = notif_msgs["payements_msgs"]
     
-    if internal_post_notif:    
-        df = get_invoices_to_notify('Proveedor', internal_post_notif)
-        invoice_expired_l   = df.sort_values(by='days_to_pay', ascending=True)
+    df                  = df_inv[(df_inv.relation=='Proveedor') & 
+                            ( (df_inv.days_to_pay.isin(internal_post_notif) | 
+                            ( (df_inv.notification_status=='notified') & (df_inv.days_to_pay < 0)))) ]
+    
+    invoice_expired_l   = df.sort_values(by='days_to_pay', ascending=True).reset_index()
                 
-        if invoice_expired_l.size:
-            html_table      =  invoice_expired_l.to_html(columns=['counterpart', 'amount', 'invoice_id', 'due_date', 'contact_email'], justify='center')
-            html_table      = format_html_table(html_table)
-            body            += paymements_msgs["invoice_expired_msg"]
-            body            += html_table
-            body            += paymements_msgs["hint_invoice_expired_msg"]
-        else: 
-            body            += paymements_msgs["no_invoice_expired_msg"]
+    if invoice_expired_l.size:
+        html_table      =  invoice_expired_l.to_html(columns=['counterpart', 'amount', 'invoice_id', 'due_date', 'contact_email'], justify='center')
+        html_table      = format_html_table(html_table)
+        body            += paymements_msgs["invoice_expired_msg"]
+        body            += html_table
+        body            += paymements_msgs["hint_invoice_expired_msg"]
+    else: 
+        body            += paymements_msgs["no_invoice_expired_msg"]
 
     return body
 
@@ -273,7 +315,8 @@ invoice_table_id        = "fili-377220.fili_demo.fili_schedule"
 notif_table_id          = "fili-377220.fili_demo.bq_notification_config"
 email_sender            = "anusmartin1@gmail.com"
 client                  = get_client();
-df_conf                 = get_configuration();
+df_config               = get_configuration();
+df_inv                  = get_pending_invoices();
 notif_msgs              = get_notification_msgs();
 internal_email_notif    = get_mails_as_list()   
 
@@ -282,8 +325,34 @@ internal_email_notif    = get_mails_as_list()
 @functions_framework.http
 def main(request):    
 
-    internal_receipt_mail = build_internal_receipt_notif();
-    send_email_smtp(internal_receipt_mail)
 
-    internal_payements_mail  = build_internal_payements_notif()
-    send_email_smtp(internal_payements_mail)
+    if (trigger_receipt_notif()):
+        internal_receipt_mail = build_internal_receipt_notif();
+        print(internal_receipt_mail["body"], '\n\n\n')        
+        send_email_smtp(internal_receipt_mail)
+        print("Receipt notification triggered")
+    else:
+        print("Receipt notification not triggered")
+
+    
+    
+    if (trigger_payement_notif()):
+        internal_payements_mail  = build_internal_payements_notif()
+        print(internal_payements_mail["body"], '\n\n\n')
+        send_email_smtp(internal_payements_mail)
+        print("Payement notification triggered")
+    else:
+        print("Payement notification not triggered")
+
+    #external_pre_notif      = get_days_as_list("external_pre_notif")
+    #external_post_notif     = get_days_as_list("external_post_notif")
+    #print("external_pre_notif:", external_pre_notif)
+    #print("external_post_notif: " ,cexternal_post_notif)
+
+    # trigger on mondays (don't forget dues on weekend)
+    # Change status to notified in BQ
+
+    return "Las notificaciones fueron enviadas!"
+
+if __name__ == "__main__":
+    main('')
