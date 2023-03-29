8ab591ff402f7c475099e0398d6558cfbb6ab3dc
Martin <manus@fi.uba.ar>
2023-03-29 10:56:22 -0300

    adds zip build for cloud funciont

---
+++ build.sh
@@ -0,0 +1,13 @@
+zip notif_config.zip            \
+    requirements.txt            \
+    credentials.json            \
+    internal_notif_msgs.json    \
+    external_notif_msgs.json    \
+    main.py                     \
+    trigger_logic.py            \
+    internal_notif_build.py     \
+    external_notif_build.py     \
+    big_query.py                \
+    utils.py                    \
+    smtp.py                     \
+    
