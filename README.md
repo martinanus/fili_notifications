eb6dda704f3a450956b93aa5065c6f886feb9aed
Martin <manus@fi.uba.ar>
2023-04-28 16:15:08 -0300

    v0.11 - adds smtp address mail check

--- README.md
+++ README.md
@@ -26,6 +26,7 @@
 
 #### Bugs
   - [x] smtp fails in cloud function if recipient doesn't exist
+  - [x] smtp fails if mail address has no correct format
   - [x] BQ query updates are not check to have run ok
    
 Diagram: 
--- smtp.py
+++ smtp.py
@@ -1,6 +1,4 @@
 import smtplib
-from email import encoders
-from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
@@ -11,6 +9,12 @@ from email.mime.text import MIMEText
 #   send_email_smtp()
 # ------------------------------------------------------------------------
 def send_email_smtp(data, smtp_sender, smtp_password):
+
+    email_to = validate_smtp_mail_address(data["email_to"])
+    if not email_to:
+        print("There is no valid email address to send mail")
+        return
+
     html_body = "<html>                 \
                     <head></head>       \
                     <body>              \
@@ -24,7 +28,7 @@ def send_email_smtp(data, smtp_sender, smtp_password):
     message = MIMEMultipart()
     message['Subject'] = data["subject"]
     message['From']     = smtp_sender
-    message['To']       = ", ".join(data["email_to"])
+    message['To']       = ", ".join(email_to)
 
 
     html_part = MIMEText(html_body, 'html')
@@ -34,6 +38,16 @@ def send_email_smtp(data, smtp_sender, smtp_password):
     s.ehlo()
     s.starttls()
     s.login(smtp_sender, smtp_password)
-    s.sendmail(smtp_sender, data["email_to"], message.as_string())
+    s.sendmail(smtp_sender, email_to, message.as_string())
     s.quit()
 
+
+def validate_smtp_mail_address(mail_address_l):
+    for mail_address in mail_address_l:
+        str_len = len(mail_address)
+        if str_len < 2:
+            mail_address_l.remove(mail_address)
+        else:
+            if (mail_address.count('@') != 1) or mail_address[0] is '@' or mail_address[str_len-1] is '@':
+                mail_address_l.remove(mail_address)
+    return mail_address_l
